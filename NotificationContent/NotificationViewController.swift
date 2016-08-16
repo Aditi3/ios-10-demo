bcd8cef4738fd28eafa9eff154b3e8e19bc8344d
Peter <peter@pwang.co>
2016-08-16 13:59:51 -0700

    add notification content extension

---
+++ NotificationContent/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotificationContent
+//
+//  Created by pwilkniss on 8/16/16.
+//  Copyright © 2016 CleverTap. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
